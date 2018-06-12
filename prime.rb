b56bceadedb542dfd422e7241bb3eccb2b473ac7
Cristina Lugo <cmlugoce@syr.edu>
2018-06-12 22:31:34 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,2 +1,11 @@
 def prime?(integer)
-  
+  if integer <= 1
+    return false
+  elsif integer == 2 || integer == 3
+    return true
+  else
+    (2..integer/2).none? do |n|
+      integer % n == 0
+    end
+  end
+end
